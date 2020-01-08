35d1990e6f322348e29cdf5b49145314b46f94e5
McKay Waters <mckaypaswaters@gmail.com>
2020-01-08 11:01:30 -0700

    finished part 10, forms

--- main.js
+++ main.js
@@ -1,3 +1,86 @@
+Vue.component('product-review', {
+    template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+
+    <p v-if='errors.length'>
+        <b> Please correct the following error(s):</b>
+        <ul>
+            <li v-for='error in errors'> {{error}}</li>
+        </ul>
+    </p>
+
+      <p>
+        <label for="name">Name:</label>
+        <input id="name" v-model="name" placeholder="name">
+      </p>
+      
+      <p>
+        <label for="review">Review:</label>      
+        <textarea id="review" v-model="review" ></textarea>
+      </p>
+      
+      <p>
+        <label for="rating">Rating:</label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+
+      <p>
+        <label for='recommend'>Would you recommend this product?(y/n)</label>
+        <select id='recommend' v-model='recommend'>
+            <option></option>
+            <option>Yes</option>
+            <option>No</option>
+        </select>
+      </p>
+          
+      <p>
+        <input type="submit" value="Submit">  
+      </p>    
+    
+    </form>
+    `,
+    data() {
+        return {
+            name: null,
+            rating: null,
+            review: null,
+            recommend: null,
+            errors: []
+        }
+    },
+    methods: {
+        onSubmit() {
+            if (this.name && this.review && this.rating){
+                let productReview = {
+                    name: this.name,
+                    review: this.review,
+                    rating: this.rating,
+                    recommend: this.recommend
+                }
+                this.$emit('review-submitted', productReview)
+                this.name = null
+                this.review = null
+                this.rating = null
+                this.recommend = null
+            }
+            else {
+                if (!this.name) this.errors.push('Name required')
+                if (!this.review) this.errors.push('Review required')
+                if (!this.rating) this.errors.push('Rating required')
+                if (!this.recommend) this.errors.push('Recommendation required')
+            }
+        }
+    }
+})
+
+
+
 Vue.component('product-details', {
     props: {
         details: {
@@ -55,6 +138,22 @@ Vue.component('product', {
                 
                 <!-- <a :href="link" target="_blank">More products like this</a> -->
             </div>
+
+            <div>
+                <h2>Reviews</h2>
+                <p v-if='!reviews.length'>There are no reviews yet</p>
+                <ul>
+                    <li v-for='review in reviews'>
+                    <p>{{ review.name }}</p>
+                    <p>Rating: {{ review.rating }}</p>
+                    <p>{{ review.review }}</p>
+                    <p>Recommended?: {{review.recommend}}</p>
+                    </li>
+                </ul>
+            </div>
+
+            <product-review @review-submitted='addReview'></product-review>
+
     </div>
     `,
     data() {
@@ -78,7 +177,8 @@ Vue.component('product', {
                     }
                 ],
                 // sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
-                onSale: true
+                onSale: true,
+                reviews: []
                 // link: 'https://www.amazon.com/s?k=green+socks&ref=nb_sb_noss'
             }
     },
@@ -94,6 +194,9 @@ Vue.component('product', {
                 },
                 updateProduct(index) {
                     this.selectedVariant = index
+                },
+                addReview(productReview) {
+                    this.reviews.push(productReview)
                 }
             },
             computed: {
